8b4c58623a2764c8def5f22623690fe11ea66747
Raushan <raushankumar606@gmail.com>
2020-11-07 00:01:00 +0530

    client side

--- server/index.js
+++ server/index.js
@@ -1,4 +1,30 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://raushan606:Raushan@123@devcamper.mzhzx.mongodb.net/memories?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`))
+  )
+  .catch((err) => console.log(err.message));
+
+mongoose.set("useFindAndModify", false);
